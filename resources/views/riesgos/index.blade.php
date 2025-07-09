5312f7cde470a795daa131417c0aaa1b75b2d46e
Cristian Lenin Roblero <cristianroblero00@gmail.com>
2025-07-08 19:44:00 -0500

    Carga de plantilla administrador

--- resources/views/riesgos/index.blade.php
+++ resources/views/riesgos/index.blade.php
@@ -1,3 +1,24 @@
-<h1>DESDE LA VISTA DE INDEX</h1>
 
-<a href="{{route('riesgos.create') }}">Crear nueva zona de riesgo</a>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+@extends('layout.administrador')
+@section('contenido')
+
+    <h1>DESDE LA VISTA DE INDEX</h1>
+    <br><br><br><br><br><br>
+    <div>
+    <a href="{{route('riesgos.create') }}">Crear nueva zona de riesgo</a>
+    </div>
+
+</body>
+</html>
+
+@endsection
