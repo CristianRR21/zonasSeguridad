485428ca6cc112e5109fc7e82fd3452a4b27a589
Eddylucero <luceroeddy8@gmail.com>
2025-07-09 01:24:44 -0500

    agregado un CDN

--- resources/views/layout/administrador.blade.php
+++ resources/views/layout/administrador.blade.php
@@ -25,6 +25,9 @@
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
 
+    <!-- Font Awesome CDN (puedes dejarlo igual si no lo descargaste local) -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+
     <!-- Nucleo Icons -->
     <link href="{{ asset('plantilla/administrador/build/assets/css/nucleo-icons.css') }}" rel="stylesheet" />
     <link href="{{ asset('plantilla/administrador/build/assets/css/nucleo-svg.css') }}" rel="stylesheet" />
